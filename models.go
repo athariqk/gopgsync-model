f0b9c32323652b5b28a1863aea9e91c5f9b9b4e4
athariqk <alib.athariq@gmail.com>
2024-07-21 21:05:49 +0700

    init: separate out models package from gopgsync

---
+++ models.go
@@ -0,0 +1,71 @@
+package gopgsyncmodel
+
+import (
+	"container/list"
+	"errors"
+	"strings"
+)
+
+type Field struct {
+	Content     interface{}
+	IsKey       bool
+	DataTypeOID uint32
+}
+
+type DmlData struct {
+	TableName string
+	// NOTE: all field names MUST BE a fully qualified name!
+	Fields map[string]Field
+}
+
+type SQLCommandType uint8
+
+const (
+	INSERT SQLCommandType = iota
+	DELETE
+	UPDATE
+)
+
+type DmlCommand struct {
+	Data    DmlData
+	CmdType SQLCommandType
+}
+
+type ReplicationMessageFlag uint8
+
+const (
+	FULL_REPLICATION ReplicationMessageFlag = iota
+	COMMIT
+)
+
+type ReplicationMessage struct {
+	TxFlag   ReplicationMessageFlag
+	Commands []*DmlCommand
+}
+
+// Flattens field map into basic column names (<table>.<field>) with plain field values
+func Flatten(columns map[string]Field, onlyKey bool) map[string]interface{} {
+	row := map[string]interface{}{}
+	for name, field := range columns {
+		if onlyKey && !field.IsKey {
+			continue
+		}
+		splits := strings.Split(name, ".")
+		if len(splits) == 2 {
+			name = splits[1]
+		}
+		row[name] = field.Content
+	}
+	return row
+}
+
+func CastToDmlCmd(e *list.Element) (*DmlCommand, error) {
+	if e == nil {
+		return nil, nil
+	}
+	dmlCommand, ok := e.Value.(*DmlCommand)
+	if !ok {
+		return nil, errors.New("incorrect casting of value to DmlCommand from queue")
+	}
+	return dmlCommand, nil
+}
